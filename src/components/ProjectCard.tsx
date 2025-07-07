b622a0cc63fb4223a19bec3d5ee65177282e7fe3
Kan <annguyen.221105sc@gmail.com>
2025-07-07 07:18:08 +0700

    Project Card outline

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,10 +1,31 @@
 type ProjectInfo = {
 	image: string,
+	name: string,
 	description: string,
 	techStack: string[],
-	link: string[]
+	link: string[],
 }
 
-export default function ProjectCard() {
-
+export default function ProjectCard({image, name, description, techStack, link}: ProjectInfo) {
+	return (
+		<div className="border-2 border-gray-200 rounded-lg">
+			<img src={image} alt={name} />
+			<h3>{name}</h3>
+			<p>{description}</p>
+			<ul>
+				{techStack.map((item, i) => (
+					<li key={i} className="bg-gray-200 rounded-lg">
+						{item}
+					</li>
+				))}
+			</ul>
+			<ul>
+				{link.map((item, i) => (
+					<li key={i} className="bg-gray-200 rounded-lg">
+						{item}
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,16 @@
+import ProjectCard from "../components/ProjectCard.tsx";
+
 export default function Projects() {
     return (
-        <>
-        </>
+        <div>
+            <h1>My projects</h1>
+            <ProjectCard
+                image={""}
+                name={"Event Management Application"}
+                description={"A fucking cunt"}
+                techStack={["React"]}
+                link={[""]}
+                />
+        </div>
     )
 }
