dfe65ec1fce4aefafe64d36b9d4322ee5445bae0
KanTheCodingNoob <s4019548@rmit.edu.vn>
2025-07-07 22:38:02 +0700

    Project Page content filled

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,28 +1,35 @@
+export type Link = {
+	title: string;
+	link: string;
+}
+
 type ProjectInfo = {
 	image: string,
 	name: string,
 	description: string,
 	techStack: string[],
-	link: string[],
+	link: Link[],
 }
 
 export default function ProjectCard({image, name, description, techStack, link}: ProjectInfo) {
 	return (
-		<div className="border-2 border-gray-200 rounded-lg">
-			<img src={image} alt={name} />
-			<h3>{name}</h3>
+		<div className="border-2 border-gray-200 rounded-lg w-76 p-5 space-y-4">
+			<img src={image} alt={name} className="w-64 h-36 shadow-md"/>
+			<h1 className="font-bold">{name}</h1>
 			<p>{description}</p>
-			<ul>
+			<ul className="flex flex-wrap gap-x-2 gap-y-1">
 				{techStack.map((item, i) => (
-					<li key={i} className="bg-gray-200 rounded-lg">
+					<li key={i} className="bg-gray-200 rounded-lg px-1">
 						{item}
 					</li>
 				))}
 			</ul>
-			<ul>
+			<ul className="flex flex-wrap">
 				{link.map((item, i) => (
-					<li key={i} className="bg-gray-200 rounded-lg">
-						{item}
+					<li key={i} className="bg-black text-white rounded-lg px-1">
+						<a href={item.link} target="_blank">
+							{item.title}
+						</a>
 					</li>
 				))}
 			</ul>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,16 +1,27 @@
 import ProjectCard from "../components/ProjectCard.tsx";
+import Footer from "../components/Footer.tsx";
 
 export default function Projects() {
     return (
-        <div>
+        <div className="flex flex-col w-full items-center space-y-4">
             <h1>My projects</h1>
-            <ProjectCard
-                image={""}
-                name={"Event Management Application"}
-                description={"A fucking cunt"}
-                techStack={["React"]}
-                link={[""]}
+            <div className="flex flex-wrap justify-between w-160">
+                <ProjectCard
+                    image={"projectImage/EventManagement.png"}
+                    name={"Event Management Application"}
+                    description={"A web application for joining, hosting and managing event."}
+                    techStack={["Typescript", "ReactJS", "TailwindCSS", "ExpressJS", "MongoDB"]}
+                    link={[{title: "Github", link: "https://github.com/Group-tam/full-stack-development"}]}
                 />
+                <ProjectCard
+                    image={"projectImage/InventoryManagement.png"}
+                    name={"Thao Inventory Management"}
+                    description={"A desktop application for local inventory management with import feature from XLSX to SQL tables."}
+                    techStack={["Typescript", "React", "Rust", "Tauri"]}
+                    link={[{title: "Github", link: "https://github.com/KanTheCodingNoob/thao-management"}]}
+                />
+            </div>
+            <Footer />
         </div>
     )
 }
