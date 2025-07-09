299087c7e604a23442da578d44cf70c33d5244b2
Kan <annguyen.221105sc@gmail.com>
2025-07-09 20:14:17 +0700

    Dark mode implemented

--- src/contexts/themeProvider.tsx
+++ src/contexts/themeProvider.tsx
@@ -1,8 +1,17 @@
-import {type ReactNode, useState} from "react";
+import {type ReactNode, useEffect, useState} from "react";
 import {type Theme, ThemeContext} from "./themeContext.ts";
 
 export default function ThemeProvider({children}: {children: ReactNode}) {
 	const [theme, setTheme] = useState<Theme>('light');
+
+	useEffect(() => {
+		const root = document.documentElement;
+		if (theme === 'dark') {
+			root.classList.add('dark');
+		} else {
+			root.classList.remove('dark');
+		}
+	}, [theme]);
 
 	return (
 		<ThemeContext.Provider value={{theme, setTheme}}>
