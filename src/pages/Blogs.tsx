c3bcbed51b57acffab9b7a6b38bf3c8c47ae03ea
Kan <annguyen.221105sc@gmail.com>
2025-07-11 05:47:40 +0700

    Finalize Blog and Contact page UI

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -5,8 +5,8 @@ import {useTranslation} from "react-i18next";
 export default function Blogs(){
     const {t} = useTranslation();
     return (
-        <div className="flex flex-col items-center w-full h-screen space-y-4">
-            <h1>{t('blogs')}</h1>
+        <div className="w-full flex flex-col items-center space-y-4">
+            <h1 className="font-bold text-4xl">{t('blogs')}</h1>
             <div className="w-148 border border-gray-700 rounded-xl divide-y py-1">
                 <BlogCard
                     title={"Launch of the portfolio website"}
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,36 +1,39 @@
 import Footer from "../components/Footer.tsx";
 import {useTranslation} from "react-i18next";
+import {useThemeContext} from "../contexts/themeContext.ts";
 
 export default function Contact() {
     const {t} = useTranslation();
+    const {theme} = useThemeContext();
 
     return (
-        <div className="flex flex-col items-center w-full h-screen space-y-4">
-            <h1>{t('contactHeader')}</h1>
-            <div className="flex space-x-4">
-                <div>
-                    <h1>{t('Contact Info')}</h1>
-                    <h2>Email: annguyen.221105sc@gmail.com</h2>
-                    <h2>{t('phone')}: (+84) 862 711 602</h2>
-                    <h2>{t('location')}: {t('actualLocation')}</h2>
-                </div>
+        <div className="w-full flex flex-col items-center space-y-4">
+            <h1 className="font-bold text-4xl">{t('contactHeader')}</h1>
+            <div className="flex space-x-4 flex-col w-full gap-4">
                 <form className="space-y-4" action="https://formspree.io/f/myzjqwdl" method="post">
-                    <h1>{t('formHeader')}</h1>
-                    <input type="name" name="name" placeholder={t('name')} className="border mr-1"/>
-                    <input type="email" name="email" placeholder="Email" className="border ml-1"/>
-                    <br />
+                    <h1 className="font-bold text-2xl">{t('formHeader')}</h1>
+                    <div className="w-full flex gap-4 justify-between">
+                        <input type="name" name="name" placeholder={t('name')} className="border w-full px-2"/>
+                        <input type="email" name="email" placeholder="Email" className="border w-full px-2"/>
+                    </div>
                     <textarea
                         name="message"
-                        className="border w-full h-20"
+                        className="border w-full h-20 px-2"
                         placeholder={t('textAreaPlaceholder')}
                     />
                     <button
                         type="submit"
-                        className="cursor-pointer"
+                        className={`cursor-pointer rounded px-2 transition duration-300 ${theme === 'dark' ? 'bg-gray-600 hover:bg-gray-900' : 'bg-gray-300 hover:bg-gray-600'}`}
                     >
                         {t('send')}
                     </button>
                 </form>
+                <div className="text-sm text-gray-600">
+                    <h1 className="font-bold">{t('contactInfo')}</h1>
+                    <h2>Email: annguyen.221105sc@gmail.com</h2>
+                    <h2>{t('phone')}: (+84) 862 711 602</h2>
+                    <h2>{t('location')}: {t('actualLocation')}</h2>
+                </div>
             </div>
             <Footer />
         </div>
