87353e52b7713e9fb0dc3a83f6567bb719d1ae27
Kan <annguyen.221105sc@gmail.com>
2025-07-11 05:09:44 +0700

    Adjust size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,15 +4,30 @@ export default function Footer(){
 	return (
 		<footer className="flex justify-between px-2 w-full py-20">
 			<p>© 2025 <a href="/">nguyenkhanhan.com</a></p>
-			<div className="space-x-3">
-				<Link to={"/resume.pdf"} target={"_blank"}>
-					Resume
+			<div className="space-x-3 flex">
+				<Link to={"https://github.com/KanTheCodingNoob"} target={"_blank"}
+					  className="px-1 py-0.5">
+					<svg viewBox="0 0 24 24" aria-hidden="true" className="size-6 fill-slate-900">
+						<path fill="currentColor" clip-rule="evenodd"
+							  d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"></path>
+					</svg>
 				</Link>
-				<Link to={"https://github.com/KanTheCodingNoob"} target={"_blank"}>
-					Github
+				<Link to={"https://www.linkedin.com/in/khanh-an-nguyen-b1433619a/"} target={"_blank"}
+					  className="px-1 py-0.5 ">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+						 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+						 className="size-6">
+						<path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
+						<rect width="4" height="12" x="2" y="9"/>
+						<circle cx="4" cy="4" r="2"/>
+					</svg>
 				</Link>
-				<Link to={"https://www.linkedin.com/in/khanh-an-nguyen-b1433619a/"} target={"_blank"}>
-					LinkedIn
+				<Link to={"mailto:annguyen.221105sc@gmail.com"} target={"_blank"}
+					  className="px-1 py-0.5">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+						<path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+						<path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+					</svg>
 				</Link>
 			</div>
 		</footer>
