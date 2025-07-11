b25bfa605e0fb6001fc2bf1d21ca4e05e307de2b
Kan <annguyen.221105sc@gmail.com>
2025-07-12 04:05:33 +0700

    Blog media issue fixed

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -9,9 +9,9 @@ type BlogCardInfo = {
 
 export default function BlogCard({title, description, postDate, url}: BlogCardInfo){
 	return (
-		<Link to={url} className="flex justify-between space-x-4 min-w-120 px-2 py-1 items-center">
+		<Link to={url} className="flex flex-col md:flex-row justify-between space-x-4 px-2 py-1 items-center">
 			<div>
-				<h1>{title}</h1>
+				<h1 className="font-bold text-2xl">{title}</h1>
 				<p>{description}</p>
 			</div>
 			<h4 className="min-w-24">{postDate}</h4>
