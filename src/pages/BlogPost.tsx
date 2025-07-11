f634aac22a3faa824e9475097ab1860ba4e3906f
Kan <annguyen.221105sc@gmail.com>
2025-07-12 04:30:57 +0700

    Add link to return to blogs

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import {Link, useParams} from "react-router-dom"
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm"
 import { useTranslation } from "react-i18next"
+import {useThemeContext} from "../contexts/themeContext.ts";
 
 export default function BlogPost() {
     const { slug } = useParams()
     const { i18n } = useTranslation();
+    const {theme} = useThemeContext();
 
     const [content, setContent] = useState("")
 
@@ -19,6 +21,14 @@ export default function BlogPost() {
 
     return (
         <div className="prose-lg">
+            <Link to={"/blogs"} className={`flex text-gray-500 gap-2 items-center ${theme === 'dark' ? 'hover:text-gray-300' : 'hover:text-gray-700'} transition duration-300 ease-in-out`}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                     stroke="currentColor" className="size-8">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"/>
+                </svg>
+                <p>Back to blogs</p>
+            </Link>
+            <img src={`/blogImages/${slug}.png`} alt="slug"/>
             <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
         </div>
     )
