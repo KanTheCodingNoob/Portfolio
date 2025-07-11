342b5e99210c56dcdc1e946ae7d8100e56ea2c40
Kan <annguyen.221105sc@gmail.com>
2025-07-12 04:47:51 +0700

    Slight update

--- src/pages/LostTraveller.tsx
+++ src/pages/LostTraveller.tsx
@@ -1,7 +1,7 @@
 export default function LostTraveller() {
     return (
-        <>
-            Hi there, seem like you are lost. Click the home button to return.
-        </>
+        <div className="w-full h-100 flex items-center justify-center">
+            <h1 className="text-center">Hi there, seem like this page do not exist. Click the home tab to return.</h1>
+        </div>
     )
 }
